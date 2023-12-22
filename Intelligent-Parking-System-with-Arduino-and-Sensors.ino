f216627532aeb5d64833524b4ee9c2628eddc965
Bogdan <76945445+Bogdan016@users.noreply.github.com>
2023-12-22 22:54:44 +0200

    updated arduino code

--- Intelligent-Parking-System-with-Arduino-and-Sensors.ino
+++ Intelligent-Parking-System-with-Arduino-and-Sensors.ino
@@ -1,5 +1,7 @@
+// Include the necessary library to work with the OLED screen
 #include "U8glib.h"
 
+// Initialize the OLED screen
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_DEV_0 | U8G_I2C_OPT_NO_ACK | U8G_I2C_OPT_FAST); // Fast I2C / TWI
 
 // 'car_image', 40x20px
@@ -185,6 +187,13 @@ const unsigned char bitmap_sound [] PROGMEM = {
 	0x01, 0xc0, 0x07, 0x01, 0x04, 0x01, 0x01, 0x0c, 0x11, 0x01, 0x1c, 0x49, 0x01, 0x3d, 0x29, 0x01, 
 	0x3d, 0x29, 0x01, 0x1c, 0x49, 0x01, 0x0c, 0x11, 0x01, 0x04, 0x01, 0x01, 0xc0, 0x07
 };
+
+// 'sound off', 24x10px
+const unsigned char bitmap_sound_off [] PROGMEM = {
+	0x01, 0xc0, 0x07, 0x01, 0x04, 0x01, 0x01, 0x0d, 0x09, 0x01, 0x1c, 0x91, 0x01, 0x3c, 0x41, 0x01, 
+	0x3c, 0x21, 0x01, 0x1c, 0x91, 0x01, 0x0d, 0x09, 0x01, 0x04, 0x01, 0x01, 0xc0, 0x07
+};
+
 // 'unit_cm', 24x10px
 const unsigned char bitmap_unit_cm [] PROGMEM = {
 	0xe0, 0x01, 0xc0, 0x80, 0x00, 0x40, 0xbe, 0xff, 0x40, 0xb6, 0xdb, 0x40, 0xb0, 0xdb, 0x40, 0xb0, 
@@ -217,109 +226,160 @@ const u8g_fntpgm_uint8_t ProjectDigits[137] U8G_FONT_SECTION("ProjectDigits") = 
   9,0,250,248,216,216,248,24,248};
 
 
-
+// Define the pins for each sensor
 #define sensor_01_ECHO_PIN 6
 #define sensor_01_TRIG_PIN 7
-
 #define sensor_02_ECHO_PIN 4
 #define sensor_02_TRIG_PIN 5
-
 #define sensor_03_ECHO_PIN 2
 #define sensor_03_TRIG_PIN 3
+#define BUZZER_PIN 8
+#define BUTTON_PIN 9  
 
-#define nrSensors 3
+#define nrSensors 3  // Number of sensors
 
+// Structure for storing data of each sensor
 struct sensor_data{
     int echo_pin;
     int trig_pin;
-    int measured_distance_cm;
-		int label_posX;
-		int label_posY;
-		int label_width;
+    int measured_distance_cm;  // Measured distance in cm
+    int label_posX;            // Label position on the X-axis
+    int label_posY;            // Label position on the Y-axis
+    int label_width;           // Label width
 };
 
-char buffer[10];
+char buffer[10];  // Buffer for storing data
 
-struct sensor_data sensor[nrSensors];
+struct sensor_data sensor[nrSensors];  // Array of structures for storing sensor data
 
-int min_dist = 10; 	// distanta minima in cm
-int max_dist = 100; // distanta maxima in cm
+int min_dist = 10; 	// Minimum distance in cm
+int max_dist = 100; // Maximum distance in cm
 
-int dist_step_1;	//distance for the first tile ( 4 tiles for each sensor)
-int dist_step_2;	//distance for the 2nd tile
-int dist_step_3;  //distance for the 3rd tile
-int dist_step_4;  //distance for the 4th tile
+// Distances for each step (there are 4 steps for each sensor)
+int dist_step_1;	
+int dist_step_2;	
+int dist_step_3;  
+int dist_step_4;  
 
-
+bool isSoundOn = true; // Declare isSoundOn and initialize it to true
+int lastButtonState = HIGH; // Declare lastButtonState and initialize it to HIGH
 
 void setup() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 
-dist_step_1 = 10;
-dist_step_2 = 25;
-dist_step_3 = 50;
-dist_step_4 = 100;
+  // Set the distances for each step
+  dist_step_1 = 10;
+  dist_step_2 = 25;
+  dist_step_3 = 50;
+  dist_step_4 = 100;
 
-
-//initialize pins for sensors
+  // Initialize the pins for sensors
   sensor[0].echo_pin = sensor_01_ECHO_PIN;
   sensor[0].trig_pin = sensor_01_TRIG_PIN;
-
   sensor[1].echo_pin = sensor_02_ECHO_PIN;
   sensor[1].trig_pin = sensor_02_TRIG_PIN;
-
   sensor[2].echo_pin = sensor_03_ECHO_PIN;
   sensor[2].trig_pin = sensor_03_TRIG_PIN;
 
+  // Set the mode of the pins for each sensor
   pinMode(sensor[0].trig_pin, OUTPUT);
   pinMode(sensor[0].echo_pin, INPUT);
-
   pinMode(sensor[1].trig_pin, OUTPUT);
   pinMode(sensor[1].echo_pin, INPUT);
-
   pinMode(sensor[2].trig_pin, OUTPUT);
   pinMode(sensor[2].echo_pin, INPUT);
 
+  // Set the position of the labels for each sensor
+  sensor[0].label_posX = 30;
+  sensor[0].label_posY = 58;
+  sensor[1].label_posX = 67;
+  sensor[1].label_posY = 58;
+  sensor[2].label_posX = 100;
+  sensor[2].label_posY = 58;
 
-	sensor[0].label_posX = 30;
-	sensor[0].label_posY = 58;
-
-	sensor[1].label_posX = 67;
-	sensor[1].label_posY = 58;
-
-	sensor[2].label_posX = 100;
-	sensor[2].label_posY = 58;
-
-
-
+  // Set the font and color for the OLED screen
   u8g.setFont(ProjectDigits);
   u8g.setColorIndex(1);
 
+  // Initialize the serial communication
   Serial.begin(115200);
 }
 
 void loop() {
 
+    int buttonState = digitalRead(BUTTON_PIN);
+    Serial.println(buttonState); // Print the state of the button
+    if(buttonState == LOW && lastButtonState == HIGH) {    // Button is pressed if the reading is LOW and it was HIGH in the last iteration
+        isSoundOn = !isSoundOn; // Toggle isSoundOn
+        delay(100); // Wait for 100ms to debounce the button
+    }
+    lastButtonState = buttonState; // Update lastButtonState
+
+    // Loop over each sensor
   for(int i = 0; i < nrSensors; i++){
+    // Trigger the sensor
     digitalWrite(sensor[i].trig_pin, HIGH);
     delayMicroseconds(10);
     digitalWrite(sensor[i].trig_pin, LOW);
 
-  // Read the result:
-   sensor[i].measured_distance_cm = pulseIn(sensor[i].echo_pin, HIGH);
-   sensor[i].measured_distance_cm = round(sensor[i].measured_distance_cm * 0.0343/2.0);
+    // Read the echo time and convert it to distance in cm
+    sensor[i].measured_distance_cm = pulseIn(sensor[i].echo_pin, HIGH);
+    sensor[i].measured_distance_cm = round(sensor[i].measured_distance_cm * 0.0343/2.0);
 
-	itoa(sensor[i].measured_distance_cm, buffer, 10);
-	sensor[i].label_width = u8g.getStrWidth(buffer);
+    // Convert the measured distance to string and calculate the label width
+    itoa(sensor[i].measured_distance_cm, buffer, 10);
+    sensor[i].label_width = u8g.getStrWidth(buffer);
+
+  if(isSoundOn==true){
+		// If the measured distance is less than 10 cm
+    if(sensor[i].measured_distance_cm < 10) {
+    // Sound the buzzer with a high frequency tone
+      tone(BUZZER_PIN, 1000, 250);
+      delay(150);  // Beep for 100ms
+      noTone(BUZZER_PIN);  // Stop the sound
   }
+    // Else if the measured distance is less than 25 cm
+    else if(sensor[i].measured_distance_cm < 25) {
+    // Sound the buzzer with a medium-high frequency tone
+      tone(BUZZER_PIN, 800, 250);
+      delay(150);  // Beep for 100ms
+      noTone(BUZZER_PIN);  // Stop the sound
+  }
+    // Else if the measured distance is less than 50 cm
+    else if(sensor[i].measured_distance_cm < 50) {
+    // Sound the buzzer with a medium-low frequency tone
+      tone(BUZZER_PIN, 600, 250);
+      delay(150);  // Beep for 100ms
+      noTone(BUZZER_PIN);  // Stop the sound
+  }
+    // Else if the measured distance is less than 100 cm
+    else if(sensor[i].measured_distance_cm < 100) {
+    // Sound the buzzer with a low frequency tone
+      tone(BUZZER_PIN, 10, 250);
+      delay(150);  // Beep for 100ms
+      noTone(BUZZER_PIN);  // Stop the sound
+  }
+  }
+}
 
-
+  // Start drawing on the display
   u8g.firstPage();
   do {
-    u8g.drawBitmapP(45, 0, 40/8, 20, bitmap_car_image); // car image on top of the display
-    u8g.drawBitmapP(1, 2, 24/8, 10, bitmap_unit_cm);		// sound on indicator 
-    u8g.drawBitmapP(103, 2, 24/8, 10, bitmap_sound);    //cm unit indicator
+    // Draw the car image, sound indicator, and unit indicator
+    u8g.drawBitmapP(45, 0, 40/8, 20, bitmap_car_image);
+    u8g.drawBitmapP(1, 2, 24/8, 10, bitmap_unit_cm);
 
+    if(isSoundOn==true) {
+    // Display the "sound on" image
+    u8g.drawBitmapP(103, 2, 24/8, 10, bitmap_sound);
+    } else {
+    // Display the "sound off" image
+    u8g.drawBitmapP(103, 2, 24/8, 10, bitmap_sound_off);
+    }
 
+    // For each sensor, draw four rectangles (one for each distance step)
+    // The rectangles are colored based on the measured distance
+    // Also draw the label with the measured distance
     u8g.drawBitmapP(26, 17, 32/8, 14, sensor[0].measured_distance_cm > dist_step_1 ? bitmap_sensor_01_a_on : bitmap_sensor_01_a_off);
     u8g.drawBitmapP(23, 25, 32/8, 16, sensor[0].measured_distance_cm > dist_step_2 ? bitmap_sensor_01_b_on : bitmap_sensor_01_b_off);
     u8g.drawBitmapP(20, 34, 32/8, 17, sensor[0].measured_distance_cm > dist_step_3 ? bitmap_sensor_01_c_on : bitmap_sensor_01_c_off);
@@ -336,16 +396,14 @@ void loop() {
     u8g.drawBitmapP(82, 43, 32/8, 18, sensor[2].measured_distance_cm > dist_step_4 ? bitmap_sensor_03_d_on : bitmap_sensor_03_d_off);
 
 	for(int i=0; i<nrSensors; i++){
-		u8g.setColorIndex(0);
-		u8g.drawBox((sensor[i].label_posX - sensor[i].label_width /2) - 2,sensor[i].label_posY - 4,sensor[i].label_width,8);
-		itoa(sensor[i].measured_distance_cm, buffer, 10);
-		u8g.setColorIndex(1);
-		u8g.drawStr(sensor[i].label_posX - sensor[i].label_width /2, sensor[i].label_posY - 3, buffer);
-	}
-
-
-
-  } while ( u8g.nextPage() );
-
-
+		// Draw a box around the label
+      u8g.setColorIndex(0);
+      u8g.drawBox((sensor[i].label_posX - sensor[i].label_width /2) - 2,sensor[i].label_posY - 4,sensor[i].label_width,8);
+      // Convert the measured distance to string
+      itoa(sensor[i].measured_distance_cm, buffer, 10);
+      // Draw the label with the measured distance
+      u8g.setColorIndex(1);
+      u8g.drawStr(sensor[i].label_posX - sensor[i].label_width /2, sensor[i].label_posY - 3, buffer);
+    }
+  } while ( u8g.nextPage() );  // Continue drawing until all pages have been displayed
 }
